599e6fd6f0dc96c37e1f9c4207db10532245e319
todesking <discommunicative@gmail.com>
2012-08-11 16:31:44 +0900

    05(recursive)

--- enumerable/05_reverse_string.rb
+++ enumerable/05_reverse_string.rb
@@ -1,3 +1,20 @@
 # -*- coding: utf-8 -*-
 # 文字列を逆順にして返す関数を作成してください。
 # ただし Array#reverse, Array#reverse! は使用禁止です。
+def reverse_without_reverse str
+  return str if str.empty?
+  return str[-1] + reverse_without_reverse(str[0..-2])
+end
+
+def ok(data)
+  expected = data.reverse
+  actual = reverse_without_reverse data
+  raise "expected #{expected} but #{actual}" unless actual == expected
+  puts "ok: #{data}"
+end
+
+ok 'aaa'
+ok ''
+ok 'abc'
+ok 'abcba'
+ok 'aaabbbccc'
