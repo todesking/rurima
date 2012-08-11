992d771496cacf0237378f596e4a13cf17c916a9
todesking <discommunicative@gmail.com>
2012-08-11 16:22:56 +0900

    03

--- enumerable/03_tap_and_map.rb
+++ enumerable/03_tap_and_map.rb
@@ -1,8 +1,8 @@
 # @option is parsed query parameter
 # e.g. { 'foo' => 'bar', 'hoge' => 'piyo'
 
-{}.tap do |hash|
-  @option.map do |k, v|
-    hash[k.to_s.camelize(:lower)] = v
-  end
-end
+require 'active_support'
+
+@option = {'fOO' => 'bar', 'Hoge' => 'piyo'}
+
+p @option.map{|k,v|[k.to_s.downcase,v]}.each_with_object({}){|(k,v),h|h[k]=v}
